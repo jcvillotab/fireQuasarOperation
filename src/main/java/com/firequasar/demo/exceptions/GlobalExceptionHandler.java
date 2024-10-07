48cd9bf5ec1289e83d95baa7621cf5f997f9fc1c
Camilo Villota <jcvillotab@unal.edu.co>
2024-10-07 07:31:28 -0500

    Updating exception handler

--- src/main/java/com/firequasar/demo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/firequasar/demo/exceptions/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.firequasar.demo.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import java.time.Instant;
@@ -23,10 +24,16 @@ public class GlobalExceptionHandler {
     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorDto.builder().message(e.getMessage()).timestamp(Instant.now()).build());
   }
 
-  @ExceptionHandler(Exception.class)
-  public ResponseEntity<ErrorDto> handleException(Exception e) {
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorDto.builder().message(e.getMessage()).timestamp(Instant.now()).build());
+  @ExceptionHandler(HttpMessageNotReadableException.class)
+  public ResponseEntity<ErrorDto> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ErrorDto.builder().message("Invalid Payload").timestamp(Instant.now()).build());
   }
 
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorDto> handleException(Exception e) {
+    e.printStackTrace();
+    System.out.println(e.getLocalizedMessage());
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorDto.builder().message(e.getMessage()).timestamp(Instant.now()).build());
+  }
 
 }
