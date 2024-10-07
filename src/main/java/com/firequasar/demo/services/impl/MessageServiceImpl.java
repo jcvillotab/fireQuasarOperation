94a6dfeda3397492c72e93f9aef56aae878ae72f
Camilo Villota <jcvillotab@unal.edu.co>
2024-10-07 00:30:57 -0500

    Modifying implementation of get message

--- src/main/java/com/firequasar/demo/services/impl/MessageServiceImpl.java
+++ src/main/java/com/firequasar/demo/services/impl/MessageServiceImpl.java
@@ -1,6 +1,8 @@
 package com.firequasar.demo.services.impl;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import com.firequasar.demo.entities.dto.SatelliteMessageDto;
 import com.firequasar.demo.services.MessageService;
@@ -8,32 +10,35 @@ import com.firequasar.demo.services.MessageService;
 @Service
 public class MessageServiceImpl implements MessageService {
 
-  public String getMessage(List<String[]> satelliteMessages) {
+  private String[] trimLeadingEmptyStrings(String[] message) {
+    int firstNonEmptyIndex = (int) Arrays.stream(message)
+        .takeWhile(String::isEmpty)
+        .count();
 
-    int maxLength = satelliteMessages.stream()
-        .mapToInt(arr -> arr.length)
+    return Arrays.copyOfRange(message, firstNonEmptyIndex, message.length);
+}
+
+  public String getMessage(List<String[]> satelliteMessages) {
+    List<String[]> trimmedMessages = satelliteMessages.stream()
+      .map(this::trimLeadingEmptyStrings)
+      .collect(Collectors.toList());
+
+    int maxMessageLength = trimmedMessages.stream()
+        .mapToInt(message -> message.length)
         .max()
         .orElse(0);
 
-    StringBuilder finalMessage = new StringBuilder();
+    String[] finalMessage = new String[maxMessageLength];
 
-    for (int i = 0; i < maxLength; i++) {
-      String word = "";
-
-      for (String[] message : satelliteMessages) {
+    for (int i = 0; i < maxMessageLength; i++) {
+      for (String[] message : trimmedMessages) {
         if (i < message.length && !message[i].isEmpty()) {
-          word = message[i];
+          finalMessage[i] = message[i];
           break;
         }
       }
-
-      if (!word.isEmpty()) {
-        finalMessage.append(word).append(" ");
-      }
     }
-
-    return finalMessage.toString().trim();
-
+    return String.join(" ", finalMessage).trim();
   }
 
   public List<String[]> convertMessages(List<SatelliteMessageDto> satellites) {
